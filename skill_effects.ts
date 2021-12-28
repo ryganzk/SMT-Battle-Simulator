83702322f020d13af966384bbf5002947d546691
anOrgandroiD <anorgandroid@outlook.com>
2021-12-28 11:40:09 -0500

    created template for additional skill effects

---
+++ skill_effects.ts
@@ -0,0 +1,8 @@
+export class SkillEffects {
+    effect: string; result: string;
+
+    constructor(effect: string, result: string) {
+        this.effect = effect;
+        this.result = result;
+    }
+}
