ac7eb952afa7e5893b22b2633ca9d14073fd1518
anOrgandroiD <anorgandroid@outlook.com>
2022-01-02 15:25:08 -0500

    created skill potential class

---
+++ skill_potential.ts
@@ -0,0 +1,17 @@
+export class SkillPotential {
+    phys: number; fire: number; ice: number; elec: number; force: number; light: number; dark: number; almi: number; ailm: number; heal: number; supp: number;
+
+    constructor(phys: number, fire: number, ice: number, elec: number, force: number, light: number, dark: number, almi: number, ailm: number, heal: number, supp: number) {
+        this.phys = phys;
+        this.fire = fire;
+        this.ice = ice;
+        this.elec = elec;
+        this.force = force;
+        this.light = light;
+        this.dark = dark;
+        this.almi = almi;
+        this.ailm = ailm;
+        this.heal = heal;
+        this.supp = supp;
+    }
+}
