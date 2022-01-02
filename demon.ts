bd8732fe0566a5221de2915506477af58ce27822
anOrgandroiD <anorgandroid@outlook.com>
2022-01-02 14:11:28 -0500

    includes type and ailment resistances

--- demon.ts
+++ demon.ts
@@ -1,12 +1,18 @@
 import {Skill} from "./skill"
+import {TypeResistances} from "./type_resistances";
+import {AilResistances} from "./ail_resistances";
 
 export class Demon {
     name: string;
     hp: number; mp: number; str: number; vit: number; mag: number; agi: number; luc: number;
     skillList: Skill[];
+    typeResistances: TypeResistances;
+    ailResistances: AilResistances;
 
 
-    constructor(name: string, hpAmt: number, mpAmt: number, strAmt: number, vitAmt: number, magAmt: number, agiAmt: number, lucAmt: number) {
+    constructor(name: string, hpAmt: number, mpAmt: number, strAmt: number, vitAmt: number, magAmt: number, agiAmt: number, lucAmt: number,
+            typeResistances: TypeResistances,
+            ailResistances: AilResistances) {
         this.name = name;
         this.hp = hpAmt;
         this.mp = mpAmt;
@@ -15,6 +21,8 @@ export class Demon {
         this.mag = magAmt;
         this.agi = agiAmt;
         this.luc = lucAmt;
+        this.typeResistances = typeResistances;
+        this.ailResistances = ailResistances;
         this.skillList = [];
     }
 
