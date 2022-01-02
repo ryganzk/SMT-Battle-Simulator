1689b68fbe4a85132719edf496c3d198f7887278
anOrgandroiD <anorgandroid@outlook.com>
2022-01-02 13:19:09 -0500

    created resistance class

---
+++ type_resistances.ts
@@ -0,0 +1,20 @@
+export class TypeResistances {
+    phys: number; fire: number; ice: number; electric: number; force: number; light: number; dark: number;
+
+    //Weak: 0
+    //Neutral: 1
+    //Resist: 2
+    //Block: 3
+    //Reflect: 4
+    //Drain: 5
+
+    constructor(phys, fire, ice, electric, force, light, dark) {
+        this.phys = phys;
+        this.fire = fire;
+        this.ice = ice;
+        this.electric = electric;
+        this.force = force;
+        this.light = light;
+        this.dark = dark;
+    }
+}
