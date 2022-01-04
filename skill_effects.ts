a36c6d38db5f2acecd81174bec04fc8b00820a8c
anOrgandroiD <anorgandroid@outlook.com>
2022-01-03 19:21:58 -0500

    removed unneeded files

--- skill_effects.ts
+++
@@ -1,8 +0,0 @@
-export class SkillEffects {
-    effect: string; result: string;
-
-    constructor(effect: string, result: string) {
-        this.effect = effect;
-        this.result = result;
-    }
-}
