bee0981b9745397104b18fc053ac3141eb8014fa
anOrgandroiD <anorgandroid@outlook.com>
2022-01-13 14:04:16 -0500

    changed skill into generic skill class

--- structures/skills/skill.ts
+++ structures/skills/skill.ts
@@ -1,25 +1,14 @@
 import {SkillEffects} from "./skill_effects";
 
 export class Skill {
-    name: string; description: string; type: string; unique!: string;
-    mpCost: number; damage: number[]; accuracy: number; iKChance: number;
-    multiTarget: boolean; randomTargets: boolean; strBased: boolean;
-    numHits: number[];
-    effects: SkillEffects[];
+    name: string; description: string; target: string; unique!: string;
+    mpCost: number;
 
-    constructor(name: string, description: string, type: string, mpCost: number, damage: number[], accuracy: number, iKChance: number, numHits: number[], effects: SkillEffects[], strBased: boolean, multiTarget: boolean, randomTargets: boolean, unique: string) {
+    constructor(name: string, description: string, mpCost: number, target: string, unique: string) {
         this.name = name;
         this.description = description;
-        this.type = type;
         this.mpCost = mpCost;
-        this.damage = damage;
-        this.accuracy = accuracy;
-        this.iKChance = iKChance;
-        this.numHits = numHits;
-        this.effects = effects;
-        this.strBased = strBased;
-        this.multiTarget = multiTarget;
-        this.randomTargets = randomTargets;
-        this.unique = unique;
+        this.target = target;
+        this.unique = unique
     }
 }
