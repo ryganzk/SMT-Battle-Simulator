a578de325a45ec072f986216bc16eb7ae55065f9
anOrgandroiD <anorgandroid@outlook.com>
2022-01-15 13:42:21 -0500

    updated compendium in accordance to changes in its subclasses

--- structures/compendium/compendium.ts
+++ structures/compendium/compendium.ts
@@ -1,24 +1,27 @@
-import {Demon} from "../characters/demon";
-import {DemonCompendium} from "./demoncompendium";
-import {Skill} from "../skills/skill";
-import {SkillCompendium} from "./skillcompendium";
-import {Item} from "../items/item";
-import {ItemCompendium} from "./itemcompendium";
+import {Demon} from "../characters/demon"
+import {DemonCompendium} from "./demoncompendium"
+import {Skill} from "../skills/skill"
+import {SkillCompendium} from "./skillcompendium"
+import {Item} from "../items/item"
+import {ItemCompendium} from "./itemcompendium"
 
 function applyMixins(derivedCtor: any, baseCtors: any[]) {
     baseCtors.forEach(baseCtor => {
         Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
              if (name !== 'constructor') {
-                derivedCtor.prototype[name] = baseCtor.prototype[name];
+                derivedCtor.prototype[name] = baseCtor.prototype[name]
             }
         });
     }); 
 }
 
 export class Compendium implements DemonCompendium, SkillCompendium, ItemCompendium {
-    getSkill!: (skillName: string) => Skill;
-    summonDemon!: (demName: string, maxLevel: number) => Demon;
-    giveItem!: (itemName: string) => Item;
+    getSkill!: (skillName: string) => Skill
+    summonDemon!: (demName: string, maxLevel: number) => Demon
+    giveItem!: (itemName: string, amount: number) => Item
+    giveShards: (amount: number) => Item[]
+    giveGems: (amount: number) => Item[]
+    giveDampeners: (amount: number) => Item[]
 }
 
-applyMixins (Compendium, [DemonCompendium, SkillCompendium, ItemCompendium]);
+applyMixins (Compendium, [DemonCompendium, SkillCompendium, ItemCompendium])
