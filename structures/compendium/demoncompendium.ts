eff5baad1f38068937f7995e16792b0738385bdd
anOrgandroiD <anorgandroid@outlook.com>
2022-01-14 20:00:51 -0500

    ability to summon demons up until level 15

--- structures/compendium/demoncompendium.ts
+++ structures/compendium/demoncompendium.ts
@@ -8,6 +8,21 @@ import {Onmoraki} from "../../demons/raptor/onmoraki"
 import {NekoShogun} from "../../demons/wargod/neko_shogun"
 import {Mandrake} from "../../demons/yoma/mandrake"
 import {Kodama} from "../../demons/jirae/kodama"
+import {CaitSith} from "../../demons/beast/cait_sith"
+import {Daemon} from "../../demons/brute/daemon"
+import {Sandman} from "../../demons/night/sandman"
+import {Erthys} from "../../demons/element/erthys"
+import {Agathion} from "../../demons/yoma/agathion"
+import {Angel} from "../../demons/divine/angel"
+import {Turdak} from "../../demons/jaki/turdak"
+import {Mermaid} from "../../demons/femme/mermaid"
+import {Obariyon} from "../../demons/haunt/obariyon"
+import {Aeros} from "../../demons/element/aeros"
+import {Tsuchigumo} from "../../demons/jirae/tsuchigumo"
+import {Bicorn} from "../../demons/wilder/bicorn"
+import {Zhen} from "../../demons/raptor/zhen"
+import {Fortuna} from "../../demons/megami/fortuna"
+import {Aquans} from "../../demons/element/aquans"
 
 export class DemonCompendium {
 
@@ -35,6 +50,52 @@ export class DemonCompendium {
             case 'kodama':
                 demon = new Kodama()
                 break
+            case 'cait sith':
+                demon = new CaitSith()
+                break
+            case 'daemon':
+                demon = new Daemon()
+                break
+            case 'sandman':
+                demon = new Sandman()
+                break
+            case 'erthys':
+                demon = new Erthys()
+                break
+            case 'agathion':
+                demon = new Agathion()
+                break
+            case 'angel':
+                demon = new Angel()
+                break
+            case 'turdak':
+                demon = new Turdak()
+                break
+            case 'mermaid':
+                demon = new Mermaid()
+                break
+            case 'obariyon':
+                demon = new Obariyon()
+                break
+            case 'tsuchigumo':
+                demon = new Tsuchigumo()
+                break
+            case 'bicorn':
+                demon = new Bicorn()
+                break
+            case 'zhen':
+                demon = new Zhen()
+                break
+            case 'aeros':
+                demon = new Aeros()
+                break
+            case 'fortuna':
+                demon = new Fortuna()
+                break
+            case 'aquans':
+                demon = new Aquans()
+                break
+            
             default:
                 throw new Error("A DEMON WITH THE NAME \"" + demName + "\" DOES NOT EXIST!")
         }
